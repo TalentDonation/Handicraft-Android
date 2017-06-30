9e614f99a0750bd3b0a85b510e6e9ad16c5e3c3e
KeonHee <kimkh6554@gmail.com>
2017-06-30 23:48:00 +0900

    Add Google Map in Location Activity

--- app/src/main/java/kr/co/landvibe/handicraft/furniture/map/LocationActivity.java
+++ app/src/main/java/kr/co/landvibe/handicraft/furniture/map/LocationActivity.java
@@ -7,16 +7,24 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import kr.co.landvibe.handicraft.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class LocationActivity extends AppCompatActivity {
-
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @BindView(R.id.toolbar_furniture_map)
     Toolbar mToolbar;
+
+    private GoogleMap mMap;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -36,5 +44,19 @@ public class LocationActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
