a19ae1ad30ac9343906df09449e98729ac5028fb
YukSangeun <tkddms150@naver.com>
2017-07-23 16:07:17 +0900

    update master name in profile

--- app/src/main/java/kr/co/landvibe/handicraft/masterProfile/MasterProfileActivity.java
+++ app/src/main/java/kr/co/landvibe/handicraft/masterProfile/MasterProfileActivity.java
@@ -55,7 +55,7 @@ public class MasterProfileActivity extends AppCompatActivity {
     private void init(){
         // Toolbar
         setSupportActionBar(mToolbar);
-        mToolbar.setTitle("김건희");
+        mToolbar.setTitle("육상은");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
