ee88ee90176ee75f0da98d6c90486d12c29948a6
YukSangeun <tkddms150@naver.com>
2017-08-17 18:32:10 +0900

    add content_introduction

--- app/src/main/java/kr/co/landvibe/handicraft/introduction/IntroductionActivity.java
+++ app/src/main/java/kr/co/landvibe/handicraft/introduction/IntroductionActivity.java
@@ -1,15 +1,35 @@
 package kr.co.landvibe.handicraft.introduction;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import kr.co.landvibe.handicraft.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class IntroductionActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
+
+        init();
+    }
+
+    private void init(){
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_intro_name);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
